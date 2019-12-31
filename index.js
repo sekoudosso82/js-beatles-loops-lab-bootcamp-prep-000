b5c8a79ca2d1877e65bd9ab76c6c61d82c7fbd66
Sekou Dosso <sekou.dosso82@gmail.com>
2019-12-31 16:31:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// add solution here
+
+function theBeatlesPlay(arrayMusicians, arrayIntruments)
+{
+  // 2
+  var emtArray = [];
+  for (var i = 0 ; i < arrayMusicians.length; i++)
+  {
+   var string = arrayMusicians[i] + ' plays ' +          arrayIntruments[i];
+    emtArray.push(string);
+  }
+  return emtArray;
+}
